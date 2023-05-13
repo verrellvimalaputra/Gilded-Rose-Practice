913250ee0f276761f286483bb188a41d77341d8d
Patrick Chiu <patrickchiu0902@gmail.com>
2023-05-13 12:56:48 +0200

    Test Succeed

--- Gilded_Rose/src/gilded_rose/Expiring.h
+++ Gilded_Rose/src/gilded_rose/Expiring.h
@@ -4,7 +4,7 @@
 
 #ifndef OOAD_23SS_DO3Y_2_CHIU_VIMALAPUTRA_EXPIRING_H
 #define OOAD_23SS_DO3Y_2_CHIU_VIMALAPUTRA_EXPIRING_H
-#include "item.h"
+#include "Item.h"
 
 class Expiring : public Item{
 public:
